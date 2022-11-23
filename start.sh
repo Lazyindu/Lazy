28a86140da79c15c24edf666651f0fcc66555c7a
indu <70087467+Lazyindu@users.noreply.github.com>
2022-11-24 00:14:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Lazyindu/LazyPrinces.git /LazyPrinces
+  git clone https://github.com/Lazyindu/Lazy.git /Lazy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrinces
+  git clone $UPSTREAM_REPO /Lazy
 fi
-cd /LazyPrinces
+cd /Lazy
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
